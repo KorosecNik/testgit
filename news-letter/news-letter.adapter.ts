144f84500fd229c3be1e7480754684d33ccc0066
Jozi <60435733+CesnjajJozek@users.noreply.github.com>
2023-04-24 13:18:57 +0200

    w4twertert

--- news-letter/news-letter.adapter.ts
+++ news-letter/news-letter.adapter.ts
@@ -7,7 +7,4 @@ export abstract class NewsLetterAdapter {
 }
 
 console.log("test1");
-console.log("test2");
-console.log("test3");
-console.log("test4");
-console.log("test5");
+console.log("fsdfsdfsdfsd");
